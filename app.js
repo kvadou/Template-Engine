3d8e845cdcdbfed9c9512fb5cfe7d0cfa8cf463b
DPK <dougkvamme@gmail.com>
2020-12-05 19:31:44 -0600

    added manager questions to app.js

--- app.js
+++ app.js
@@ -10,7 +10,48 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
+let teamList = [];
+const managerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "Enter manager name:",
+    validate: async (input) => {
+      if (input == "" || /\s/.test(input)) {
+        return "Please enter first or last name.";
+      }
+      return true;
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter manager's email:",
+    validate: async (input) => {
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)) {
+        return true;
+      }
+      return "Please enter a valid email address.";
+    },
+  },
+  {
+    type: "input",
+    name: "officeNum",
+    message: "Enter office number:",
+    validate: async (input) => {
+      if (isNaN(input)) {
+        return "Please enter a number";
+      }
+      return true;
+    },
+  },
+  {
+    type: "list",
+    name: "hasTeam",
+    message: "Do you have any team members?",
+    choices: ["Yes", "No"],
+  },
+];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
